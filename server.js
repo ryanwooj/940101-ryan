2562158285d04b7299b716adacaa98fde51130fe
Ryan Woo <devrwoo@gmail.com>
2019-01-28 00:08:16 -0800

    Change port to 5000

--- server.js
+++ server.js
@@ -16,6 +16,10 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profiles'));
 app.use('/api/posts', require('./routes/api/posts'));
 
+app.get('/', function(req, res) {
+  res.send('hello world');
+});
+
 //Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   //Set static folder
