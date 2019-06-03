5fb855bbee719764d4a0dad432f5fb0ac21f82ae
Ryan Woo <devrwoo@gmail.com>
2019-06-03 02:59:27 -0700

    Post Component notifications

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -8,7 +8,6 @@ import CommentForm from './CommentForm';
 import CommentItem from './CommentItem';
 import { getPost } from '../../actions/post';
 import Button from '@material-ui/core/Button';
-import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
 import Paper from '@material-ui/core/Paper';
 import { makeStyles } from '@material-ui/core/styles';
